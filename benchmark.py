b970c9b694318154ce6329faffe609ac057008cc
unknown <ramvard@gmail.com>
2020-09-25 22:34:43 +0530

    refactor: benchmark.py

--- benchmark.py
+++ benchmark.py
@@ -3,7 +3,7 @@ from agents.agentsList import benchmarkList as agents
 from games.gamesList import benchmarkList as games
 import os
 
-NUMBER_OF_GAMES_PER_PAIR_OF_AGENTS = 5
+NUMBER_OF_GAMES = 5
 
 parameters = [{
     'name': 'C',
@@ -18,16 +18,16 @@ os.system('cls')
 
 for game in games:
     print('\nGame:', game)
-    for agent1 in agents:
-        for agent2 in agents:
-            if agent1 == agent2:
-                continue
+    for i in range(len(agents)):
+        for j in range(i+1, len(agents)):
+            agent1 = agents[i]
+            agent2 = agents[j]
             wins1 = 0
             wins2 = 0
             draws = 0
             print('Running:', agent1, 'vs', agent2)
-            for i in range(NUMBER_OF_GAMES_PER_PAIR_OF_AGENTS):
-                displayPrefix = 'Game {0}/{1}'.format(i+1, NUMBER_OF_GAMES_PER_PAIR_OF_AGENTS)
+            for i in range(NUMBER_OF_GAMES):
+                displayPrefix = 'Game {0}/{1}'.format(i+1, NUMBER_OF_GAMES)
                 gameInstance = Game(agent1, parameters, agent2, parameters, game, displayPrefix)
                 winner = gameInstance.playGame(displayState=False)
                 if winner == 1:
@@ -36,4 +36,16 @@ for game in games:
                     wins2 += 1
                 else:
                     draws += 1
-            print('Result: {0}: {1}% {2}: {3}% Draws: {4}%'.format(agent1, wins1*20, agent2, wins2*20, draws*20))
+
+            print('Running:', agent2, 'vs', agent1)
+            for i in range(NUMBER_OF_GAMES):
+                displayPrefix = 'Game {0}/{1}'.format(i+1, NUMBER_OF_GAMES)
+                gameInstance = Game(agent2, parameters, agent1, parameters, game, displayPrefix)
+                winner = gameInstance.playGame(displayState=False)
+                if winner == 1:
+                    wins2 += 1
+                elif winner == -1:
+                    wins1 += 1
+                else:
+                    draws += 1
+            print('Result: {0}: {1}% {2}: {3}% Draws: {4}%'.format(agent1, (wins1*50)/NUMBER_OF_GAMES, agent2, (wins2*50)/NUMBER_OF_GAMES, (draws*50)/NUMBER_OF_GAMES))
