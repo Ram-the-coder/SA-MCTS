9f6a5f5754c5e6ab7957668b1a91e310b150ac3b
unknown <ramvard@gmail.com>
2020-10-05 15:15:35 +0530

    refactor(benchmark): get benchmark options from user

--- benchmark.py
+++ benchmark.py
@@ -1,40 +1,12 @@
 from game import Game
+from agents.agentsList import benchmarkList as agents
 import agents.agentsList as agentsList
 from games.gamesList import benchmarkList as games
 from math import inf
 import os
-import threading
 
-# Return 1 if agent1 wins, 0 if draw, -1 if agent2 wins
-def runGame(agent1, agent2, game, parameters, displayPrefix):
-    gameInstance = Game(agent1, parameters, agent2, parameters, game, displayPrefix)
-    winner = gameInstance.playGame(displayState=False)
-    return winner
-
-threadLock = threading.Lock()
-
-class myThread(threading.Thread):
-    def __init__(self, agent1, agent2, game, parameters, displayPrefix, stats):
-        threading.Thread.__init__(self)
-        self.agent1 = agent1
-        self.agent2 = agent2
-        self.parameters = parameters
-        self.game = game
-        self.displayPrefix = displayPrefix
-        self.stats = stats
-
-    def run(self):
-        winner = runGame(self.agent1, self.agent2, self.game, self. parameters, self.displayPrefix)    
-        threadLock.acquire()
-        if winner == 1:
-            self.stats[0] += 1
-        elif winner == -1:
-            self.stats[1] += 1
-        else:
-            self.stats[2] += 1
-        threadLock.release()
-
-NUMBER_OF_GAMES = 10
+#################### CONSTANTS ####################
+# HALF_NUMBER_OF_GAMES_PER_PAIR = 5
 
 parameters = [{
     'name': 'C', # MCTS Exploration Constant
@@ -59,33 +31,112 @@ parameters = [{
     'default': 0.01
 }]
 
-os.system('cls')
-agents = agentsList.benchmarkList
+#################### Methods ####################
+def benchmarkAll(HALF_NUMBER_OF_GAMES_PER_PAIR):
+    for game in games:
+        print('----------------------------------------\nGame:', game)
+        for i in range(len(agents)):
+            agent1 = agentsList.mcts
+            agent2 = agents[i]
+            if agent2 == agentsList.mcts:
+                continue
+            wins1 = 0
+            wins2 = 0
+            draws = 0
+            print('Running:', agent1, 'vs', agent2)
+            for k in range(HALF_NUMBER_OF_GAMES_PER_PAIR):
+                displayPrefix = 'Game {0}/{1}'.format(k+1, HALF_NUMBER_OF_GAMES_PER_PAIR)
+                gameInstance = Game(agent1, parameters, agent2, parameters, game, displayPrefix)
+                winner = gameInstance.playGame(displayState=False)
+                if winner == 1:
+                    wins1 += 1
+                elif winner == -1:
+                    wins2 += 1
+                else:
+                    draws += 1
 
-for game in games:
-    print('\nGame:', game)
-    for i in range(len(agents)):
-        agent1 = agentsList.mcts
-        agent2 = agents[i]
-        if agent2 == agentsList.mcts:
-            continue
+            print('Running:', agent2, 'vs', agent1)
+            for k in range(HALF_NUMBER_OF_GAMES_PER_PAIR):
+                displayPrefix = 'Game {0}/{1}'.format(k+1, HALF_NUMBER_OF_GAMES_PER_PAIR)
+                gameInstance = Game(agent2, parameters, agent1, parameters, game, displayPrefix)
+                winner = gameInstance.playGame(displayState=False)
+                if winner == 1:
+                    wins2 += 1
+                elif winner == -1:
+                    wins1 += 1
+                else:
+                    draws += 1
+            print('Result: {0}: {1}/{5} {2}: {3}/{5} Draws: {4}/{5}\n'.format(agent1, wins1, agent2, wins2, draws, HALF_NUMBER_OF_GAMES_PER_PAIR*2))
 
-        print('Running:', agent1, 'vs', agent2)
-        threads = []
-        stats = [0, 0, 0] #[agent1Wins, agent2Wins, draws]
-        for k in range(NUMBER_OF_GAMES):
-            displayPrefix = 'Game {0}/{1}'.format(k+1, NUMBER_OF_GAMES)
-            thread = myThread(agent1, agent2, game, parameters, displayPrefix, stats)
-            thread.start()
-            threads.append(thread)
+def singleTunerBenchmark(HALF_NUMBER_OF_GAMES_PER_PAIR):
+    print('Choose the allocation strategy that you want to benchmark:')
+    print('1. Naive Monte Carlo (NMC)')
+    print('2. Evolutionary Algorithm (EA)')
+    print('3. Covariance Matrix Adaptation Evolution Strategy (CMA-ES)')
 
-        for k in range(NUMBER_OF_GAMES):
-            displayPrefix = 'Game {0}/{1}'.format(k+1, NUMBER_OF_GAMES)
-            thread = myThread(agent2, agent1, game, parameters, displayPrefix, stats)
-            thread.start()
-            threads.append(thread)
+    choice = int(input())
+    agent1 = agentsList.mcts
+    agent2 = agentsList.nmc
+
+    if choice == 1:
+        agent2 = agentsList.nmc
+    elif choice == 2:
+        agent2 = agentsList.ea
+    elif choice == 3:
+        agent2 = agentsList.cmaes
+
+    print('Chosen allocation strategy:', agent2)
+     
+    for game in games:
+        print('----------------------------------------\nGame:', game)
+
+        wins1 = 0
+        wins2 = 0
+        draws = 0
         
-        for thread in threads:
-            thread.join()
+        print('Running:', agent1, 'vs', agent2)
+        for k in range(HALF_NUMBER_OF_GAMES_PER_PAIR):
+            displayPrefix = 'Game {0}/{1}'.format(k+1, HALF_NUMBER_OF_GAMES_PER_PAIR)
+            gameInstance = Game(agent1, parameters, agent2, parameters, game, displayPrefix)
+            winner = gameInstance.playGame(displayState=False)
+            if winner == 1:
+                wins1 += 1
+            elif winner == -1:
+                wins2 += 1
+            else:
+                draws += 1
 
-        print('Result: {0}: {1}% {2}: {3}% Draws: {4}%'.format(agent1, (stats[0]*50)/NUMBER_OF_GAMES, agent2, (stats[1]*50)/NUMBER_OF_GAMES, (stats[2]*50)/NUMBER_OF_GAMES))
+        print('Running:', agent2, 'vs', agent1)
+        for k in range(HALF_NUMBER_OF_GAMES_PER_PAIR):
+            displayPrefix = 'Game {0}/{1}'.format(k+1, HALF_NUMBER_OF_GAMES_PER_PAIR)
+            gameInstance = Game(agent2, parameters, agent1, parameters, game, displayPrefix)
+            winner = gameInstance.playGame(displayState=False)
+            if winner == 1:
+                wins2 += 1
+            elif winner == -1:
+                wins1 += 1
+            else:
+                draws += 1
+        print('Result: {0}: {1}/{5} {2}: {3}/{5} Draws: {4}/{5}\n'.format(agent1, wins1, agent2, wins2, draws, HALF_NUMBER_OF_GAMES_PER_PAIR*2))
+
+
+
+#################### Main Method ####################
+def main():
+    os.system('cls')
+    
+    numberOfGames = int(input('Enter the number of games to benchmark against:'))
+    HALF_NUMBER_OF_GAMES_PER_PAIR = numberOfGames // 2
+
+    print('1. Benchmark all online allocation strategies')
+    print('2. Benchmark a single allocation strategies')
+    choice = int(input('Choose:'))
+    
+    if(choice == 1):
+        benchmarkAll(HALF_NUMBER_OF_GAMES_PER_PAIR)
+    else:
+        singleTunerBenchmark(HALF_NUMBER_OF_GAMES_PER_PAIR)
+
+#################### Call to Main Method ####################
+if __name__ == '__main__':
+    main()
