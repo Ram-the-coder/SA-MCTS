d68d206429d12c598f5798dda9b5d3856392eb53
Sudharshan <sudharshanv2018@gmail.com>
2020-11-22 23:22:22 +0530

    add MAST and GRAVE

--- agents/mcts.py
+++ agents/mcts.py
@@ -1,4 +1,5 @@
 from random import choice
+from random import random
 from math import log, sqrt
 
 X_PLAYER = 1
@@ -7,7 +8,7 @@ TIE = -1
 ONGOING = 0
 
 class Node:
-    def __init__(self, state, depth, move=None):
+    def __init__(self, state, depth, move=None,parent=None):
         self.state = state
         self.children = []
         self.isTerminal = False
@@ -15,13 +16,17 @@ class Node:
         self.wins = 0
         self.depth = depth
         self.move = move
-        self.root = None
+        self.parent = parent
 
 
 class MonteCarlo:
     def __init__(self, board, **kwargs):
         self.board = board
         self.params = {}
+        self.lookup={}
+        self.AmafTable={}
+        self.f=open("output.txt","w")
+
 
     def setRootNodeState(self, state):
         self.root = Node(state, 0)
@@ -32,6 +37,7 @@ class MonteCarlo:
 
         # Selection
         path = self.select(self.root)
+        # print("Path   ",path[0].children)
 
         # Expansion
         leaf = path[0]
@@ -42,12 +48,23 @@ class MonteCarlo:
         opponent = O_PLAYER if player == X_PLAYER else O_PLAYER
         reward = 0
         # if not leaf.isTerminal:
-        reward, terminalDepth = self.playout(leaf, player)
+        reward, terminalDepth,move = self.playout(leaf, player)
+        # print(self.lookup)
 
         # Backpropagation
         for node in path:
             node.plays += 1
             node.wins += reward
+            if str(node) in self.AmafTable:
+                if str(move) in self.AmafTable[str(node)]:
+                    obj=self.AmafTable[str(node)][str(move)]
+                    obj['plays']+=1
+                    obj['wins']+=1 if reward==1 else 0
+                    obj['average']=obj['wins']/obj['plays']
+                else:
+                    self.AmafTable[str(node)][str(move)]={"plays":0,"wins":0,"average":0.0}
+            else:
+                self.AmafTable[str(node)]={}
         
         return reward
 
@@ -57,8 +74,21 @@ class MonteCarlo:
     def select(self, node):
         if len(node.children) == 0 or node.plays < self.params['T']:
             return [node]
+        total=max(sum(child.plays for child in node.children), 1)
+        log_total = log(total)
+        K=self.params['K']
+        Beta=sqrt(K/(3*total+K))
+        Ref=self.params['Ref']
+        temp=node
+        ancestor=node #closest ancestor that has atleast ref visits
+        while temp!=None:
+            visits=max(sum(child.plays for child in temp.children), 1)
+            if visits>=Ref:
+                ancestor=temp
+                break
+            temp=temp.parent
         
-        log_total = log(max(sum(child.plays for child in node.children), 1))
+        
 
         if len(node.children) < 1:
             raise Exception('length less than one')
@@ -67,16 +97,18 @@ class MonteCarlo:
         bestScore = -1000000
 
         for child in node.children:
-            score = (child.wins / (child.plays + 1)) + (self.params['C'] * sqrt(log_total/(child.plays+1)))
+            score=(1-Beta)*(child.wins / (child.plays + 1))+ (self.params['C'] * sqrt(log_total/(child.plays+1)))
+            score+=Beta*self.AmafTable[ancestor][node.move] if ancestor in self.AmafTable else 0 
             if score > bestScore:
                 bestScore = score
 
         goodChildren = []
         for child in node.children:
-            score = (child.wins / (child.plays + 1)) + (self.params['C'] * sqrt(log_total/(child.plays+1)))
+            score = (1-Beta)*(child.wins / (child.plays + 1))+(self.params['C'] * sqrt(log_total/(child.plays+1)))
+            score+=Beta*self.AmafTable[ancestor][node.move] if ancestor in self.AmafTable else 0
             if bestScore - self.params['VO'] <= score:
                 goodChildren.append(child)
-
+        self.f.write(str(len(goodChildren))+'\n')
         selectedChild = choice(goodChildren)
 
         if selectedChild == None:
@@ -91,7 +123,8 @@ class MonteCarlo:
         if self.board.winner(node.state) != 0:
             node.isTerminal = True
             return
-        node.children = [Node(self.board.next_state(node.state, move), node.depth + 1, move) for move in self.board.legal_plays(node.state)]
+        # print(node.state,self.board.legal_plays(node.state))
+        node.children = [Node(self.board.next_state(node.state, move), node.depth + 1, move,node) for move in self.board.legal_plays(node.state)]
             
     
     # Simulation Logic
@@ -102,17 +135,41 @@ class MonteCarlo:
         winner = self.board.winner(node.state)
         if winner != ONGOING:
             if winner == TIE:
-                return (0.5, node.depth)
+                return (0.5, node.depth,node.move)
             
-            return (1 if winner == player else 0, node.depth)
+            return (1 if winner == player else 0, node.depth,node.move)
 
-        selectedChild = None
+        # selectedChild = None
         if len(node.children) > 0:
-            selectedChild = choice(node.children)
+            selectedChild = self.choose(node.children)   #select a node from a list of nodes
         else:
-            selectedChild = choice([Node(self.board.next_state(node.state, move), node.depth + 1, move) for move in self.board.legal_plays(node.state)])
+            selectedChild = self.choose([Node(self.board.next_state(node.state, move), node.depth + 1, move) for move in self.board.legal_plays(node.state)])
+        selectedChild.move=repr(selectedChild.move)
+        if selectedChild.move in self.lookup:    
+            self.lookup[selectedChild.move]['plays']+=1
+            self.lookup[selectedChild.move]['average']=self.lookup[selectedChild.move]['wins']/self.lookup[selectedChild.move]['plays']
+        else:
+            self.lookup[selectedChild.move]={'plays':0,'wins':0,'average':0.0}
+        result,depth,move=self.playout(selectedChild, player)
+        if result==1:
+            self.lookup[selectedChild.move]['wins']+=1
+        return result,depth,selectedChild.move
 
-        return self.playout(selectedChild, player)
+    def choose(self,options):
+        if random()<self.params['eps'] or len(self.lookup)<2:
+            return choice(options)
+        else:
+            #sort lookup table based on values
+            self.lookup={k: v for k, v in sorted(self.lookup.items(), key=lambda item:item[1]['average'])}
+            for i in self.lookup:
+                for j in options:
+                    if j.move==i:
+                        return j
+            return choice(options)
+
+
+
+
 
     def setParams(self, params):
         for param in params:
